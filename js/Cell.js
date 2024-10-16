4a16b0d1f59241f90b556af16894e3b12e6fae7c
Cole deboer <Pg27Cole@vfs.com>
2024-10-15 18:21:13 -0700

    Fixed The bugs I made in the last Commit

--- js/Cell.js
+++ js/Cell.js
@@ -1,32 +1,27 @@
 import CellTypes from "/js/CellTypes.js";
-
-// this is the type that changes at runtime
-let type;
 export default class Cell 
 {
     constructor(cell, cellType)
     {
-        //this is the type that is constant.
         this.cellType = cellType;
-        type = cellType;
         this.cell = cell;
     }
 
     CellClicked()
     {
-        if(type === CellTypes.Flag)
-        {
-            //auido or something
-        }
-        if(type === CellTypes.Safe)
-        {
-            this.CheckSoroundings();
-        }
-        if(type === CellTypes.Bomb)
+        if(this.cellType === CellTypes.Bomb)
         {
             alert("LOST");
             location.reload();
         } 
+        if(this.cellType === CellTypes.Flag)
+        {
+            //auido or something
+        }
+        if(this.cellType === CellTypes.Safe)
+        {
+            this.CheckSoroundings();
+        }
     }
 
     CellFlagged()
@@ -34,16 +29,16 @@ export default class Cell
         let flag = document.createElement('img');
         flag.src = "/images/Flag.jpg";
         flag.className = "cell"
-        if(type === 0)
+        if(this.cellType === CellTypes.Safe)
         {
             this.cell.removeChild(flag);
         }
-        if(type === CellTypes.Safe)
+        if(this.cellType === CellTypes.Safe)
         {
-            type = CellTypes.Safe;
+            this.cellType = CellTypes.Safe; //THIS DOESNT WORK BECAUSE ITS VALUES PASSED THROUGH PARAMTER ARE CONSTANT.
             this.cell.appendChild(flag);
         }
-        if(type === CellTypes.Bomb && this.cellType)
+        if(this.cellType === CellTypes.Bomb)
         {
             console.log(this.cellType);
         }
